43cabfff8b67e2df8a06014677e60e2275ac0a7a
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2023-02-15 18:59:56 -0300

    balance

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,14 @@
 import Header from '../../components/Header'
+import Balance from '../../components/Balance';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function Home() {
   return (
     <View style={styles.container}>
-        <Header />
-        <Text>Pagina Home!!</Text>
+        <Header name ="Lucas Cortinhas"/>
+        <Balance saldo="9.250,90" gastos="-527,00"/>
+
+        <Text style={styles.title}>Últimas movimentações</Text>
     </View> 
   );
 }
@@ -13,6 +16,13 @@ export default function Home() {
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#fafafa',
     },
+    title:{
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginLeft: 14,
+      marginRight: 14,
+      marginTop: 14,
+    }
 });
