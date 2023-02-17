0931c293b1a69886fee6b472964c9eba112d024b
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2023-02-16 22:03:45 -0300

    animation

--- src/components/Actions/index.js
+++ src/components/Actions/index.js
@@ -25,6 +25,20 @@ export default function Actions() {
         </View>
         <Text style={styles.labelButton}>Carteira</Text>
     </TouchableOpacity>
+
+    <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+            <AntDesign name="barcode" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Boletos</Text>
+    </TouchableOpacity>
+
+    <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+            <AntDesign name="setting" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Conta</Text>
+    </TouchableOpacity>
    </ScrollView>
   );
 }
--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -1,9 +1,25 @@
 import { Text, View, StyleSheet } from "react-native";
 import React, { Component } from "react";
+import { MotiView } from "moti";
 
 export default function Balance({saldo, gastos}) {
   return (
-    <View style={styles.container}>
+    <MotiView 
+    style={styles.container}
+    from={{
+      rotateX: '-100deg',
+      opacity: 0,
+    }}
+    animate={{
+      rotateX: '0deg',
+      opacity: 1,
+    }}
+    transition={{
+      type: "timing",
+      delay: 300,
+      duration: 900
+    }}
+    >
       <View style={styles.item}>
         <Text style={styles.itemTitle}>Saldo</Text>
         <View style={styles.content}>
@@ -19,7 +35,7 @@ export default function Balance({saldo, gastos}) {
             <Text style={styles.expense}>{gastos}</Text>
         </View>
       </View>
-    </View>
+    </MotiView>
   );
 }
 
